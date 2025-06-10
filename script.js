8abc647aa34a28dec53550e99ea33b9e48871d1f
lizreznik <525ereznik@frhsd.com>
2025-06-10 10:17:06 -0400

    finally added the video

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function myFunction() {
         btn.innerHTML = "Play";
     }
 }
+$(document).ready(function () {
+    $('#picture img').each(function () {
+        var description = $(this).data('description');
+
+        $(this).hover(function () {
+            $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
+        }, function () {
+            $(this).next('.description').fadeOut().remove();
+        });
+    })
+});
