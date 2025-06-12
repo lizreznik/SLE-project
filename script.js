439ed8c7dd09669fbb499a2cfdbd7c9e3de9600a
lizreznik <144464805+lizreznik@users.noreply.github.com>
2025-06-12 23:03:16 +0000

    upcoming shows section updated + heading section changed (no more video)

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
 
-function myFunction() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Play";
-    }
-}
 $(document).ready(function () {
     $('#picture img').each(function () {
         var description = $(this).data('description');
@@ -21,3 +10,28 @@ $(document).ready(function () {
         });
     })
 });
+
+ const shows = [
+      { date: 'February 4, 2025', name: 'The Vanguard Jazz Orchestra', time: 'The Village Vanguard; 8:00/10:00' }, 
+      { date: 'February 10, 2025', name: 'The Vanguard Jazz Orchestra', time: 'The Village Vanguard; 8:00/10:00' },
+      { date: 'February 20, 2025', name: 'Tatum Greenblatt Quintet', time: 'Smalls; 7:30/9:00' },
+      { date: 'June 20, 2025', name: 'Daddy Rabbit', time: '...'},
+    ];
+
+    const tourList = document.getElementById('tourList');
+
+    shows.forEach(show => {
+      const item = document.createElement('div');
+      item.className = 'show-item';
+
+      item.innerHTML = `
+        <div class="show-date">${show.date}</div>
+        <div class="show-details">
+          <div class="show-city">${show.name}</div>
+          <div class="show-venue">${show.time}</div>
+        </div>
+      `;
+
+      tourList.appendChild(item);
+
+    }); 
